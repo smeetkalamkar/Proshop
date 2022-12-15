1ce099fc8cc55ab28678d89b09d0e235023d92dd
smeetkalamkar <97446803+smeetkalamkar@users.noreply.github.com>
2022-12-15 14:44:42 +0530

    Select quantity feaure and cart routes added

--- frontend/src/screens/MainRoutes.jsx
+++ frontend/src/screens/MainRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
+import CartScreen from './CartScreen'
 import HomeScreen from './HomeScreen'
 import ProductScreen from './ProductScreen'
 const MainRoutes = () => {
@@ -7,6 +8,8 @@ const MainRoutes = () => {
     <Routes>
     <Route path="/"  element={<HomeScreen/>} exact />
     <Route path={`/product/:id`}  element={<ProductScreen/>}  />
+    <Route path={`/cart/:id`} element={<CartScreen/>} />
+    <Route path={`/cart`} element={<CartScreen/>} />
     </Routes>
   )
 }
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect } from "react";
 import {useDispatch,useSelector} from "react-redux"
-import { Link, useParams } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem ,Form} from "react-bootstrap";
 import Rating from "../components/Rating";
 import { listProductDetails } from "../actions/productActions";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { useState } from "react";
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = () => {
   const { id } = useParams();
+  
+  const [qty,setQty] = useState(1)
   const dispatch = useDispatch();
+  const navigate = useNavigate()
   const productDetails = useSelector(state=>state.productDetails);
   const {loading,error,product} = productDetails
   useEffect(()=>{
 
    dispatch(listProductDetails(id))
   },[dispatch,id])
+
+  const addToCartHandler = ()=>{
+        navigate(`/cart/${id}?qty=${qty}`)
+  }
   return (
     <>
       <Link to="/" className="btn btn-light my-3">
@@ -70,9 +78,31 @@ const ProductScreen = ({ match }) => {
          </Col>
        </Row>
      </ListGroup.Item>
-
+    {
+      product.countInStock >0 && (
+        <ListGroup.Item>
+          <Row>
+            <Col>Qty</Col>
+            <Col>
+            <Form.Control
+            as={"select"}
+            value={qty}
+            onChange={(e)=>{
+              setQty(e.target.value)
+            }}
+            >
+          {[...Array(product.countInStock).keys()].map((x)=>{
+            return <option key={x+1} value={x+1}>{x+1}</option>
+          })
+          }
+            </Form.Control>
+            </Col>
+          </Row>
+        </ListGroup.Item>
+      )
+    }
      <ListGroup.Item>
-       <Button   disabled={product.countInStock ===0}>
+       <Button   disabled={product.countInStock ===0}  onClick={addToCartHandler}>
          Add To Cart
        </Button>
      </ListGroup.Item>
