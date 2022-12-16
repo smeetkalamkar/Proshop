af45ef5fb645fbf7846c8405ecc043eef6f1a5d3
smeetkalamkar <97446803+smeetkalamkar@users.noreply.github.com>
2022-12-16 18:59:35 +0530

    created cart reducer ,add to cart action and modified store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,17 @@ import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartRedeucers";
 const reducer = combineReducers({
   productList :productListReducer,
-  productDetails:productDetailsReducer
+  productDetails:productDetailsReducer,
+  cart:cartReducer
 });
-const initialState = {};
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')?JSON.parse(localStorage.getItem('cartItems')):[];
+const initialState = {
+  cart:{carItems:cartItemsFromStorage}
+};
 const middleware = [thunk];
 
 const store = legacy_createStore(
