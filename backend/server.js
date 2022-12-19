5dce6643e4cbb9a163fd1a958592b43a81915e26
smeetkalamkar <97446803+smeetkalamkar@users.noreply.github.com>
2022-12-19 14:23:32 +0530

    backend user authentication implemented

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 import colors from "colors"
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js"
 import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 const app = express();
+app.use(express.json())  // allow us to accept json data in body
 dotenv.config();
 connectDB()
 
@@ -14,7 +16,7 @@ res.send("App is running")
 })
 
 app.use('/api/products',productRoutes)
-
+app.use('/api/users',userRoutes)
 app.use(notFound)
 
 
