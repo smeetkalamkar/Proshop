70d0f896d6effc2d60b16ef6a9dddd243307f655
smeetkalamkar <97446803+smeetkalamkar@users.noreply.github.com>
2023-02-02 18:48:25 +0530

    user registration & password encryption done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,40 @@ const authUser = asyncHandler(async (req, res) => {
 });
 
 
+// @desc  register user & get token
+// @route  POST /api/users/register
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name,email, password } = req.body;
+
+  const userExists = await User.findOne({email});
+  
+  if(userExists){
+    res.send(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if(user){
+    res.status(201).json({
+      _id:user._id,
+      name:user.name,
+      email:user.email,
+      isAdmin:user.isAdmin,
+      token:generateToken(user._id)
+    })
+  }
+  else{
+    res.status(400)
+    throw new Error('Invalid  Credentials')
+  }
+});
+
 // @desc  Get user profile
 // @route  GET /api/users/profile
 // @access Private
@@ -52,4 +86,4 @@ else{
 
 });
 
-export { authUser ,getUserProfile};
+export { authUser ,getUserProfile,registerUser};
